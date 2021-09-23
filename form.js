48983b414579c9c7554ed45e8509ca0ec80c6767
Diego C <diegocs2410@gmail.com>
2021-09-22 22:48:14 -0500

    Funcion terminada con la expresion regular adecuada

--- form.js
+++ form.js
@@ -1,5 +1,18 @@
 //  Bienvenidos equipo !!
 
-const checkCorreo = (valor) => {};
+const checkCorreo = (valor) => {
+    // Espresion regular para testear la condicion del parametro valor
+    // Recuerden que debe retornar true or false
+    const expReg =
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (expReg.test(valor)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+// Ejecutar node form.js para verificar en la consola true or false
+console.log(checkCorreo('Diego@diego.com')); // Retorna True
 // Esta es la manera de exportar las funciones que estemos realizando
 module.exports = { checkCorreo };
