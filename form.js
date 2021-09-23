cf75584461e1e132ff9b2d3b466d59317fd731c4
Memelkin <57773246+Memelkin@users.noreply.github.com>
2021-09-23 04:44:44 -0500

    Update form.js

--- form.js
+++ form.js
@@ -1,1 +1,19 @@
-//  Bienvenidos equipo !!
+//Validar password
+const checkContrasena = (valor) => {
+    
+    //Exprecion para verificar contraseña
+    const exprecion_password = 
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d$@$!%*?&]{8}/i;
+        
+    if (exprecion_password.test(valor)) {
+	    return true;
+    } else {
+        return false;
+    }
+};
+
+//Validación en true para password
+console.log(checkContrasena('123456aA'));
+
+//Exportar
+module.exports = { checkContrasena };
