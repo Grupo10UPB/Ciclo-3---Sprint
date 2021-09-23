2d3eec33c998a0911ffc4c145a209d350daf5773
Diego C <diegocs2410@gmail.com>
2021-09-22 22:29:57 -0500

    Manera como debemos exportar las funciones

--- form.js
+++ form.js
@@ -1,1 +1,6 @@
 //  Bienvenidos equipo !!
+console.log('hahahah');
+
+const checkCorreo = () => {};
+// Esta es la manera de exportar las funciones que estemos realizando
+module.exports = { checkCorreo };
