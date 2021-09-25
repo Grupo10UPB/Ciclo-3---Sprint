04d581e669ab1a6074a7e29d730e298eb874b623
Diego C <diegocs2410@gmail.com>
2021-09-24 20:46:57 -0500

    Funciones probadas correctamente

--- form.js
+++ form.js
@@ -1,18 +1,45 @@
 //  Bienvenidos equipo !!
-
-const checkCorreo = (valor) => {
-    // Espresion regular para testear la condicion del parametro valor
-    // Recuerden que debe retornar true or false
-    const expReg =
-        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if (expReg.test(valor)) {
-        return true;
-    } else {
-        return false;
-    }
+// Objeto con las expresiones regulares
+const expresiones = {
+  nombre: /^[a-zA-ZÀ-ÿ\s]{4,30}$/, // Letras y espacios, pueden llevar acentos.
+  password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  telefono: /^\d{7}$/, // 7 numeros.
 };
+// Funcion checkCorreo
+const checkCorreo = (valor) => {
+  // Espresion regular para testear la condicion del parametro valor
+  // Recuerden que debe retornar true or false
+  const expReg =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return expReg.test(valor) ? true : false;
+};
+// Funcion Check Nombre
+const checkNombre = function (valor) {
+  const expReg = new RegExp(/^[a-zA-ZÀ-ÿ\s]{4,30}$/);
 
-// Ejecutar node form.js para verificar en la consola true or false
-console.log(checkCorreo('Diego@diego.com')); // Retorna True
-// Esta es la manera de exportar las funciones que estemos realizando
-module.exports = { checkCorreo };
+  return expReg.test(valor) ? true : false;
+};
+// Funcion checkApellido
+const checkApellido = function (valor) {
+  const expReg = new RegExp(/^[a-zA-ZÀ-ÿ\s]{4,30}$/);
+
+  return expReg.test(valor) ? true : false;
+};
+// Funcion checkTelefono
+const checkTelefono = function (valor) {
+  if (Number.isInteger(valor)) {
+    const newValor = valor.toString().split('');
+    if (newValor.length === 7) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
+// Funcion checkContraseña
+const checkContrasena = (valor) => (expresiones.password.test(valor) ? true : false);
+// Exportar modulos
+module.exports = { checkCorreo, checkApellido, checkNombre, checkTelefono, checkContrasena };
