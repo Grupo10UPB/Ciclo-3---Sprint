c9690362c709f235baa3185efb3777aa269b1058
Grupo10UPB <91229574+Grupo10UPB@users.noreply.github.com>
2021-09-25 13:55:35 -0500

    Verión final de form.js
    Codigo completado para Sprint 2

--- form.js
+++ form.js
@@ -1,1 +1,42 @@
-//  Bienvenidos equipo !!
+// Objeto con las expresiones regulares
+const expresiones = {
+  nombre: /^[a-zA-ZÀ-ÿ\s]{4,30}$/, // Expresion funcional para campos nombre y apellido. Recibe solo caracteres letras, pueden ser con acento, espacios, longitud restringida entre 4 - 30 caracteres, sin opcion de campo vacio.
+  telefono: /^\d{7}$/, // Expresion funcional para campo telefono. Recibe solo caracteres numericos, longitud restringida de 7 caracteres, sin opcion de campo vacio.
+  correo: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i || /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, // Expresion funcional para campo correo. Recibe estructura de correo ej: correo@correo.com, sin opcion de campo vacio.
+  password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, //  Expresion funcional para campo contrasena. Recibe caracteres letras y numeros, con minimo una Mayuscula una minuscula y un numero con una longitud igual o mayor a 8 caracteres, sin opcion de campo vacio.
+  
+  
+};
+
+// Funcion Check Nombre
+const checkNombre = function (valor) {
+  // Expresion regular para testear la condicion del parametro valor
+  const expReg = new RegExp(/^[a-zA-ZÀ-ÿ\s]{4,30}$/);
+    return expReg.test(valor) ? true : false;
+};
+  
+// Funcion checkApellido
+const checkApellido = function (valor) {
+  // Expresion regular para testear la condicion del parametro valor
+  const expReg = new RegExp(/^[a-zA-ZÀ-ÿ\s]{4,30}$/);
+    return expReg.test(valor) ? true : false;
+};
+ 
+// Funcion checkTelefono
+const checkTelefono = (valor) => (expresiones.telefono.test(valor) ? true : false); //  Testeo de la condicion del parametro valor, a traves de el llamado del objeto telefono, donde reside la expresion regular.
+
+// Funcion checkCorreo
+const checkCorreo = (valor) => {
+  // Expresion regular para testear la condicion del parametro valor
+  const expReg =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+      return expReg.test(valor) ? true : false;
+};
+  
+// Funcion checkContraseña
+const checkContrasena = (valor) => (expresiones.password.test(valor) ? true : false); //  Testeo de la condicion del parametro valor, a traves de el llamado del objeto password, donde reside la expresion regular.
+
+// Cada función retorna true or false según los requisitos solicitados en el sprint 2.
+
+// Exportar modulos
+module.exports = {checkNombre, checkApellido, checkTelefono, checkCorreo, checkContrasena};
